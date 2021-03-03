75d72e4fee108dcddc239bcbec472ba880de029c
zhongrui <zhongrui@021.com>
2021-03-03 17:41:57 +0800

    file

---
+++ TreeView/src/main/java/com/github/treeview/TreeNode.java
@@ -0,0 +1,33 @@
+package com.github.treeview;
+
+import java.util.List;
+
+public class TreeNode {
+    private String id;
+    private String name;
+    private List<TreeNode> children;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<TreeNode> getChildren() {
+        return children;
+    }
+
+    public void setChildren(List<TreeNode> children) {
+        this.children = children;
+    }
+}
